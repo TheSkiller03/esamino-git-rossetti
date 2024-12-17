0e7e251f14d38158c0d77c8e6003d7c4f317cbae
TheSkiller03 <theskiller243@gmail.com>
2024-12-17 14:45:40 +0100

    feat: aggiunta funzione deleteTodo

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ function addTodo(title, category) {
     console.log('Todo aggiunto:', newTodo);
 }
 
+function deleteTodo(id) {
+    todos = todos.filter(todo => todo.id !== id);
+    console.log('Todo eliminato, ID:', id);
+}
+
+
